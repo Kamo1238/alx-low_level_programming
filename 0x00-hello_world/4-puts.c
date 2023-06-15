1c0bfa5e83d220ae8c14429e4d94e334a3acaa88
kamo1238 <kmomabotha@gmail.com>
2023-06-15 14:28:14 -0700

    betty 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#intlude <stdio.h> 
+/**
+ * main - A program that prints a line with puts function 
+ * Return: 0 (success)
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");return(0);}
+
