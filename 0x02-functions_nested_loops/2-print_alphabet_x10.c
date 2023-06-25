629dadc2acc4466b0da992e21f508befa8843f3e
kamo1238 <kmomabotha@gmail.com>
2023-06-25 09:16:02 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,21 @@
 /**
  * print_alphabet_x10 - Make alphabet x10 times
  * Return: void
+ *
  */
 
 void print_alphabet_x10(void)
+
 {
 char ch;
 int i;
 i = 0;
 while (i < 10)
-{for (ch = 'a'; ch <= 'z'; ch++)
-{_putchar(ch);}
-_putchar('\n'); i++;}
-
-} 
-
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n'); i++;
+}
+}
